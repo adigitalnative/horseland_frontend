c1aeb992f3c6e5488ca18d530287c9329548f298
Jacqueline Chenault <adigitalnative@gmail.com>
2018-11-29 22:26:22 -0500

    Display gender on horse card

--- src/Components/HorseCard.js
+++ src/Components/HorseCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Card, Image, Button} from 'semantic-ui-react'
+import {Card, Image, Button, Icon} from 'semantic-ui-react'
 
 const HorseCard = ({horse, onClick, singleHorse}) => {
   return(
     <Card>
       <Image src="https://via.placeholder.com/200" />
       <Card.Content>
-        <Card.Header>{horse.name}</Card.Header>
+        <Card.Header>
+          {horse.name}
+          {horse.gender === "female" ? <Icon name="venus" /> : <Icon name="mars" />}
+
+        </Card.Header>
         <Card.Meta>Age: {horse.age}</Card.Meta>
-        <Card.Description>{horse.name} is a {horse.breed} horse.</Card.Description>
+        <Card.Description>{horse.name} is a {horse.color} {horse.breed} horse.</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Button fluid size="tiny" onClick={() => onClick(horse)}>
